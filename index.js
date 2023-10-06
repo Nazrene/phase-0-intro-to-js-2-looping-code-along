ca478433c58b2e23d0a5a04a70985b541571232e
Naz <mercy.kamau@student.moringaschool.com>
2023-10-06 16:44:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+
+     for (let i = 0;i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(message);
+     }
+     
+     return thankYouMessages;
+}
+    
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+
+const messages = writeCards(namesArray, eventName)
+
+
+console.log(messages);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
 // Code your solutions in this file
